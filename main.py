1ad857b4ff266d8e032a400d4dbbfccb193ffb68
anurag <anuragsawle70@gmail.com>
2020-09-26 11:11:50 +0530

    Adding GetMatches Function

--- main.py
+++ main.py
@@ -8,11 +8,4 @@ app = Flask(__name__)
 def getMatches():
     c = Cricbuzz()
     matches = c.matches()
-    a = []
-    for match in matches:
-        d = {'series': match['srs'],
-             'team1': match['team1']['name'],
-             'team2': match['team2']['name'],
-             'status': match['status']}
-        a.append(d)
-    return jsonify(a)
+    return jsonify(matches)
